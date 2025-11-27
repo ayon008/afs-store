cc43c3f391872eb8491cbe2cc4937e9da8893965
ayon008 <shariar.ayon128@gmail.com>
2025-11-27 14:38:48 +0600

    added

--- src/app/afs-team/page.jsx
+++ src/app/afs-team/page.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 const BreadCums = () => {
     return (
-        <div className='uppercase absolute top-6 z-20 global-padding left-6'>
+        <div className='uppercase'>
             <div className='font-bold text-sm text-[#999999]'>
                 <Link className='inline' href={'/'}>Accueil</Link> / <span className='text-black'> L’équipe</span>
             </div>
@@ -22,19 +22,21 @@ const BreadCums = () => {
 
 const page = () => {
     return (
-        <div className='bg-white global-padding relative'>
-            <div className='global-padding-top w-full relative global-margin'>
+        <div className='bg-white global-padding relative pt-4'>
+            <div>
                 <BreadCums />
-                <h1 className='global-h1 text-center relative mt-0'>L'équipe Foil And Co.</h1>
-            </div>
-            <div className='flex items-start gap-10 global-margin'>
-                <div className='w-[60%] relative'>
-                    <Image src={teamImage} alt='Our Team' className='mx-auto rounded-md' />
+                <div className='lg:my-[80px] my-[40px]'>
+                    <h1 className='global-h1 text-center relative'>L'équipe Foil And Co.</h1>
                 </div>
-                <div className='mt-10 w-[40%]'>
-                    <div className='max-w-[520px]'>
-                        <h2 className='text-[36px] font-bold leading-[110%] tracking-[-0.01em]'>Des collaborateurs animés par une même passion</h2>
-                        <p className='text-lg font-semibold mt-6'>Chez Foil And Co., notre équipe partage une passion commune pour l’innovation et l’excellence. Chaque membre contribue à faire avancer notre mission avec engagement, créativité et expertise, afin de vous offrir des produits et services de qualité. Découvrez les visages de ceux qui oeuvrent à la production de vos équipements favoris.</p>
+                <div className='flex items-start gap-10 global-margin'>
+                    <div className='w-[60%] relative'>
+                        <Image src={teamImage} alt='Our Team' className='mx-auto rounded-md aspect-[] object-cover' />
+                    </div>
+                    <div className='pt-10 w-[40%]'>
+                        <div className='max-w-[520px]'>
+                            <h2 className='text-[36px] font-bold leading-[110%] tracking-[-0.01em]'>Des collaborateurs animés par une même passion</h2>
+                            <p className='text-lg font-semibold mt-6'>Chez Foil And Co., notre équipe partage une passion commune pour l’innovation et l’excellence. Chaque membre contribue à faire avancer notre mission avec engagement, créativité et expertise, afin de vous offrir des produits et services de qualité. Découvrez les visages de ceux qui oeuvrent à la production de vos équipements favoris.</p>
+                        </div>
                     </div>
                 </div>
             </div>
