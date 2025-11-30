c22cc9809e657396ddd968571263aca0cfc0c6ba
ayon008 <shariar.ayon128@gmail.com>
2025-12-01 01:59:45 +0600

    added

--- src/app/map/Dealers.jsx
+++ src/app/map/Dealers.jsx
@@ -79,8 +79,17 @@ const Dealers = () => {
         id: i, lat: parseFloat(singleData?.acf?.latitude), lng: parseFloat(singleData?.acf?.longitude)
     }));
 
-    console.log(locations);
+    const customMarkerSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <rect x="11" y="8" width="10" height="11" fill="white"/>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M27 13.4545C27 22.3636 16 30 16 30C16 30 5 22.3636 5 13.4545C5 10.4166 6.15893 7.5031 8.22183 5.35496C10.2847 3.20681 13.0826 2 16 2C18.9174 2 21.7153 3.20681 23.7782 5.35496C25.8411 7.5031 27 10.4166 27 13.4545ZM16 17.2727C18.025 17.2727 19.6667 15.5633 19.6667 13.4545C19.6667 11.3458 18.025 9.63636 16 9.63636C13.975 9.63636 12.3333 11.3458 12.3333 13.4545C12.3333 15.5633 13.975 17.2727 16 17.2727Z" fill="#1D98FF" stroke="#111111" stroke-width="0.1" stroke-linecap="round"/>
+</svg>
+`;
 
+    const customIcon = {
+        url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent(customMarkerSvg),
+        scaledSize: new window.google.maps.Size(32, 32),
+    };
 
 
     return (
@@ -100,7 +109,10 @@ const Dealers = () => {
                     }}
                 >
                     {locations.map((loc) => (
-                        <Marker key={loc.id} position={{ lat: loc.lat, lng: loc.lng }} />
+                        <Marker key={loc.id} position={{ lat: loc.lat, lng: loc.lng }} icon={{
+                            url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent(customMarkerSvg),
+                            scaledSize: new window.google.maps.Size(32, 32),
+                        }} />
                     ))}
                 </GoogleMap>
                 <div className='z-20 top-8 left-4 absolute max-w-[385px] w-full bg-transparent h-[380px]'>
